eefce51eaa86542857188ba5eb535d25d748c96b
KoreanBadboy <giang_hoang@hotmail.com>
2026-04-30 15:01:28 +0200

    Setup EF Core, DbContext, and initial migration

--- LibraryApp.Domain/Entities/Book.cs
+++ LibraryApp.Domain/Entities/Book.cs
@@ -6,4 +6,5 @@ public class Book
     public string Title { get; set; }
     public string Author { get; set; }
     public DateTime PublishDate { get; set; }
+    public int UserId { get; set; }
 }
