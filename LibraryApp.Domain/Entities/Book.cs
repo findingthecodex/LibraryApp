9db4346c814162ba0bc3ccdcb89e56cb03e0c973
KoreanBadboy <giang_hoang@hotmail.com>
2026-04-30 14:51:04 +0200

    Fixed: cleaner architecture with domain outisde API. User, book and quote entities added

---
+++ LibraryApp.Domain/Entities/Book.cs
@@ -0,0 +1,9 @@
+namespace LibraryApp.Domain.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public DateTime PublishDate { get; set; }
+}
---
+++ LibraryApp.Domain/Entities/Quote.cs
@@ -0,0 +1,9 @@
+namespace LibraryApp.Domain.Entities;
+
+public class Quote
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string Author { get; set; }
+    public int UserId { get; set; }
+}
